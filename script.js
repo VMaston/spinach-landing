331d61452c2c365c942fd716754f57e46b52bb98
Vincent Maston <vincentmaston@outlook.com>
2022-01-22 02:33:56 +0000

    redesign of the page to better support scrollmagic

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 var controller = new ScrollMagic.Controller();
 
-// build scene
 new ScrollMagic.Scene({
     triggerElement: "#trigger1",
-    triggerHook: 0.6, // show, when scrolled 10% into view
-    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+    triggerHook: 0.6,
     offset: 50 // move trigger to center of element
 })
-    .setClassToggle(".about-block1", "reveal") // add class to reveal
-    //.addIndicators() // add indicators (requires plugin)
+    .setClassToggle(".about-block1", "reveal")
+    .reverse(false)
+    //.addIndicators()
     .addTo(controller);
 
 new ScrollMagic.Scene({
     triggerElement: "#trigger2",
-    triggerHook: 0.6, // show, when scrolled 10% into view
-    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-    offset: 50 // move trigger to center of element
+    triggerHook: 0.6,
+    offset: 50
 })
-    .setClassToggle(".about-block2", "reveal") // add class to reveal
-    //.addIndicators() // add indicators (requires plugin)
+    .setClassToggle(".about-block2", "reveal")
+    .reverse(false)
+    //.addIndicators()
     .addTo(controller);
 
 new ScrollMagic.Scene({
     triggerElement: "#trigger3",
-    triggerHook: 0.6, // show, when scrolled 10% into view
-    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-    offset: 50 // move trigger to center of element
+    triggerHook: 0.6,
+    offset: 50
 })
-    .setClassToggle(".about-block3", "reveal") // add class to reveal
-    //.addIndicators() // add indicators (requires plugin)
+    .setClassToggle(".about-block3", "reveal")
+    .reverse(false)
+    //.addIndicators()
     .addTo(controller);
