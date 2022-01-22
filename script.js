0e7bb80c167cd1de4254db52eb853426cd492ae9
Vincent Maston <vincentmaston@outlook.com>
2022-01-22 13:00:06 +0000

    put seperate calls into a loop

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
 var controller = new ScrollMagic.Controller();
 
-new ScrollMagic.Scene({
-    triggerElement: "#trigger1",
-    triggerHook: 0.6,
-    offset: 50 // move trigger to center of element
-})
-    .setClassToggle(".about-block1", "reveal")
-    .reverse(false)
-    //.addIndicators()
-    .addTo(controller);
+document.querySelectorAll('.about-block').forEach(elem => {
 
-new ScrollMagic.Scene({
-    triggerElement: "#trigger2",
-    triggerHook: 0.6,
-    offset: 50
-})
-    .setClassToggle(".about-block2", "reveal")
-    .reverse(false)
-    //.addIndicators()
-    .addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: elem,
+        triggerHook: 0.6,
+        offset: 50 // move trigger to center of element
+    })
+        .setClassToggle(elem, "reveal")
+        .reverse(false)
+        //.addIndicators()
+        .addTo(controller)
 
-new ScrollMagic.Scene({
-    triggerElement: "#trigger3",
-    triggerHook: 0.6,
-    offset: 50
-})
-    .setClassToggle(".about-block3", "reveal")
-    .reverse(false)
-    //.addIndicators()
-    .addTo(controller);
+});
