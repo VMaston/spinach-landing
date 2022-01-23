ed87d041dacc6d6c3654e00e7bd2b92b6ccb6b14
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 17:51:33 +0000

    using XMLHttpRequest technique

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,7 +1,22 @@
 exports.handler = async function(event, context) {
 
-        let response = await fetch(`https://expensemate-staging.netlify.app/api/companies?name=${companyName}`);
-        let data = await response.json();
-        return data;
-        
+    var HttpClient = function() {
+        this.get = function(aUrl, aCallback) {
+            var anHttpRequest = new XMLHttpRequest();
+            anHttpRequest.onreadystatechange = function() { 
+                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                    aCallback(anHttpRequest.responseText);
+            }
+    
+            anHttpRequest.open( "GET", aUrl, true );            
+            anHttpRequest.send( null );
+        }
+    }
+
+    var client = new HttpClient();
+client.get(`https://expensemate-staging.netlify.app/api/companies?name=${companyName}`, function(response) {
+    let data = await response.json()
+    return data
+});
+
 }
