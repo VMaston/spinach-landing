642226c2dfc00bd268323bc70f0e681d5106638f
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 17:41:32 +0000

    move axios import to js file

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,5 +1,9 @@
 exports.handler = async function(event, context) {
 
+    var axiosload = document.createElement('script');
+    axiosload.setAttribute('src', 'https://unpkg.com/axios/dist/axios.min.js')
+    document.head.appendChild(axiosload);
+
     axios({
         url: `https://expensemate-staging.netlify.app/api/companies?name=${companyName}`,
         method: 'get'
