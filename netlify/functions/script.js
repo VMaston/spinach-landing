9ca95acbecf8bbedf963d2a548a947c0db5d0fc5
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 18:13:53 +0000

    script rewrite

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -2,14 +2,26 @@ const axios = require('axios');
 
 exports.handler = async (event, context) => {
 
-    axios({
-        url: `https://expensemate-staging.netlify.app/api/companies?name=spinach`,
-        method: 'get'
-    })
-        .then(function (response) {
-            result = JSON.stringify(response.data);
+    const url = `https://expensemate-staging.netlify.app/api/companies?name=spinach`;
+    console.log("URL: ", url)
+
+    return axios({
+        method: "get",
+        url: url,
+      })
+        .then((response) => {
+          console.log("data", response.data);
+          return {
+            statusCode: 200,
+            body: JSON.stringify(response.data),
+          };
         })
-        .catch(function (error) {
-            console.log(error)
+        .catch((error) => {
+          console.log(error);
+          return {
+            statusCode: 500,
+            body: JSON.stringify(error.message),
+          };
         });
+
 }
