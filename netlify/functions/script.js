1f0de64b418e67d6ef7b17ee802d6b3eeb6aeac2
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 18:59:25 +0000

    script update for testing

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -2,22 +2,19 @@ const axios = require('axios');
 
 exports.handler = async (event, context) => {
 
-    const url = `https://expensemate-staging.netlify.app/api/companies?name=${event.queryStringParameters.url}`;
-    console.log("URL: ", url)
+    const url = `https://expensemate-staging.netlify.app/api/companies?name=${event.queryStringParameters.url}?limit=5`;
 
     return axios({
         method: "get",
         url: url,
       })
         .then((response) => {
-          console.log("data", response.data);
           return {
             statusCode: 200,
             body: JSON.stringify(response.data),
           };
         })
         .catch((error) => {
-          console.log(error);
           return {
             statusCode: 500,
             body: JSON.stringify(error.message),
