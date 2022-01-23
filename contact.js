44c63915fbd8a31a4d88b5a0bd26b59e57528e0b
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 22:21:08 +0000

    attempted autolist function

---
+++ contact.js
@@ -0,0 +1,37 @@
+function autocomplete(arr) {
+    
+    var search = document.getElementById("cname");
+
+    search.addEventListener("input", function() {
+
+        var container, listItems, i;
+        container = document.createElement("DIV");
+        container.setAttribute("id", this.id + "autocomplete-list");
+        container.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(container);
+
+        for (i = 0; i < arr.length; i++) {
+            listItems = document.createElement("DIV");
+            listItems.innerHTML = arr[i];
+            listItems.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                listItems.addEventListener("click", function() {
+                search.value = this.getElementsByTagName("input")[0].value;
+            });
+            container.appendChild(listItems);
+          
+        }
+    });
+  } 
+
+
+async function mySubmit() {
+
+    let companyResult;
+
+    var companyName = document.getElementById("cname").value;
+    await fetch(`/.netlify/functions/script?url=${companyName}`)
+        .then(response => response.json())
+        .then(data => companyResult = data.items)
+
+        return companyResult
+}
