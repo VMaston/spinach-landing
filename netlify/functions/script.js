2c3d33c3e8fcbf2d694a437990290cf5e9315091
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 17:56:21 +0000

    fetch again

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,22 +1,7 @@
 exports.handler = async function(event, context) {
 
-    var HttpClient = async function() {
-        this.get = function(aUrl, aCallback) {
-            var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function() { 
-                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                    aCallback(anHttpRequest.responseText);
-            }
-    
-            anHttpRequest.open( "GET", aUrl, true );            
-            anHttpRequest.send( null );
-        }
-    }
-
-    var client = new HttpClient();
-client.get(`https://expensemate-staging.netlify.app/api/companies?name=${companyName}`, async function(response) {
-    let data = await response.json()
-    return data
-});
-
+    fetch(`https://expensemate-staging.netlify.app/api/companies?name=${companyName}`)
+    .then((response) => {
+      return response.json();
+    })
 }
