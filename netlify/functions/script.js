e24c9eb197b86a8c26e0c0a66549729975bf166b
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 17:47:13 +0000

    drop axios for ES7 fetch

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,22 +1,7 @@
 exports.handler = async function(event, context) {
 
-    var axiosload = document.createElement('script');
-    axiosload.setAttribute('src', 'https://unpkg.com/axios/dist/axios.min.js')
-    document.head.appendChild(axiosload);
-
-    axios({
-        url: `https://expensemate-staging.netlify.app/api/companies?name=${companyName}`,
-        method: 'get'
-    })
-        .then(function (response) {
-            result = JSON.stringify(response.data);
-        })
-        .catch(function (error) {
-            console.log(error)
-        });
-
-    return {
-        statusCode: 200,
-        body: result
-    }
+        let response = await fetch(`https://expensemate-staging.netlify.app/api/companies?name=${companyName}`);
+        let data = await response.json();
+        return data;
+        
 }
