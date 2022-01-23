1afa31678264cf9728219609700ea3f2422ffcfb
Vincent Maston <vincentmaston@outlook.com>
2022-01-23 18:00:52 +0000

    axios...again

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,7 +1,20 @@
-exports.handler = async function(event, context) {
+import * as axios from 'https://unpkg.com/axios/dist/axios.min.js';
 
-    fetch(`https://expensemate-staging.netlify.app/api/companies?name=${companyName}`)
-    .then((response) => {
-      return response.json();
+export async function handler(event, context) {
+
+    axios({
+        url: `https://expensemate-staging.netlify.app/api/companies?name=${companyName}`,
+        method: 'get'
     })
+        .then(function (response) {
+            result = JSON.stringify(response.data);
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
+
+        let response = await fetch(`https://expensemate-staging.netlify.app/api/companies?name=${companyName}`);
+        let data = await response.json();
+        return data;
+        
 }
