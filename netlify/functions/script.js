8a6da6820c05bba340dfda192bfb36a9dd40757f
Vincent Maston <vincentmaston@outlook.com>
2022-01-24 00:13:38 +0000

    switch from axiom to nodefetch

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,24 +1,17 @@
-const axios = require('axios');
+const fetch = require("node-fetch");
 
 exports.handler = async (event, context) => {
+  const url = `https://expensemate-staging.netlify.app/api/companies?name=${event.queryStringParameters.url}`;
 
-    const url = `https://expensemate-staging.netlify.app/api/companies?name=${event.queryStringParameters.url}`;
-
-    return axios({
-        method: "get",
-        url: url,
-      })
-        .then((response) => {
-          return {
-            statusCode: 200,
-            body: JSON.stringify(response.data),
-          };
-        })
-        .catch((error) => {
-          return {
-            statusCode: 500,
-            body: JSON.stringify(error.message),
-          };
-        });
-
-}
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return { statusCode: 200, body: JSON.stringify({ data }) };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed fetching data" }),
+    };
+  }
+};
