577e64b3756900371b50442bfc98070abdf706c7
Vincent Maston <vincentmaston@outlook.com>
2022-01-24 00:30:01 +0000

    node-fetch v2 for package.json

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 exports.handler = async (event, context) => {
   const url = `https://expensemate-staging.netlify.app/api/companies?name=${event.queryStringParameters.url}`;
