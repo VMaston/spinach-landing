3d650b8187a37e042d319df59c6c2a6ce5c7fb83
Vincent Maston <vincentmaston@outlook.com>
2022-01-24 01:54:24 +0000

    scrollmagic prettier

--- scrollmagic.js
+++ scrollmagic.js
@@ -1,33 +1,31 @@
 var controller = new ScrollMagic.Controller();
 
-document.querySelectorAll('.about-block').forEach(elem => {
-
-    new ScrollMagic.Scene({
-        triggerElement: elem,
-        triggerHook: 0.6,
-        offset: 50 // move trigger to center of element
-    })
-        .setClassToggle(elem, "reveal")
-        .reverse(false)
-        //.addIndicators()
-        .addTo(controller)
-
+document.querySelectorAll(".about-block").forEach((elem) => {
+  new ScrollMagic.Scene({
+    triggerElement: elem,
+    triggerHook: 0.6,
+    offset: 50, // move trigger to center of element
+  })
+    .setClassToggle(elem, "reveal")
+    .reverse(false)
+    //.addIndicators()
+    .addTo(controller);
 });
 
 new ScrollMagic.Scene({
-    triggerElement: '.contact',
-    triggerHook: 0.05,
-    offset: 0
+  triggerElement: ".contact",
+  triggerHook: 0.05,
+  offset: 0,
 })
-    .setClassToggle('.logo', "black")
-    //.addIndicators()
-    .addTo(controller)
+  .setClassToggle(".logo", "black")
+  //.addIndicators()
+  .addTo(controller);
 
 new ScrollMagic.Scene({
-    triggerElement: '.main',
-    triggerHook: 0.05,
-    offset: -80
+  triggerElement: ".main",
+  triggerHook: 0.05,
+  offset: -80,
 })
-    .setClassToggle('.logo', "dark")
-    //.addIndicators()
-    .addTo(controller)
+  .setClassToggle(".logo", "dark")
+  //.addIndicators()
+  .addTo(controller);
